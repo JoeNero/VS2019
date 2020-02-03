3156cf248537f4ce6724b9e29d094678d633d38f
526988861@qq.com <526988861@qq.com>
2020-02-03 14:23:09 +0800

    Signed-off-by: 526988861@qq.com <526988861@qq.com>

--- 容器demo/mList.cpp
+++ 容器demo/mList.cpp
@@ -25,7 +25,6 @@
 
 时间	:	2020/01/08
 */
-
 #include <iostream>
 #include <algorithm>
 #include <list>
@@ -58,25 +57,23 @@ bool ComparePerson(Person&, Person&);
 
 void test01();
 void test02();
+void test03();
 
 void printList(const list<int>&);			//打印
 void printList(const list<Person>&);			//打印
 
-
-
 int main()
 {
 
-	test01();
-	test02();
+//	test01();
+//	test02();
+	test03();
 	system("pause");
 	return 0;
 }
 
 /*
 功能: 	- list容器
-		@param
-		@param
 		@param
 描述	:
 示例	:
@@ -89,45 +86,43 @@ void test01()
 	{
 		L1.push_back(rand() % 100);
 	}
+	cout << "L1 = " << endl;
 	printList(L1);
 
 	list<int> L2(L1.begin(), L1.end());
+	cout << "L2 = " << endl;
 	printList(L2);
 
 	list<int> L3(L2);
+	cout << "L3 = " << endl;
 	printList(L3);
 
+	cout << "L4 = " << endl;
 	list<int> L4(6, 11);
 	printList(L4);
 
 	L1.swap(L4);
 	cout << "交换" << endl;
+	cout << "L1 = " << endl;
 	printList(L1);
+	cout << "L4 = " << endl;
 	printList(L4);
 	L4.reverse();
 	cout << "反转";
+	cout << "L4 = " << endl;
 	printList(L4);
 	cout << "排序";
+	cout << "L4 = " << endl;
 	L4.sort();
 	printList(L4);
 	cout << "降序";
 	L4.sort(cmp);
+	cout << "L4 = " << endl;
 	printList(L4);
-
-
-}
-
-
-
-bool cmp(int a, int b)
-{
-	return a > b;
 }
 
 /*
 功能: 	- test02()
-		@param
-		@param
 		@param
 描述	:
 示例	:
@@ -163,6 +158,76 @@ void test02()
 	L.sort(ComparePerson);
 	printList(L);
 
+}
+
+void test03()
+{
+	list<int> L1;
+	list<int> L2;
+	srand((unsigned int)time(NULL));
+	int* num = new int[5];
+	//初始化L1
+	for (int i = 0; i < 5; i++)
+	{
+		num[i] = rand() % 100;
+		L1.push_back(i);
+		//L1.push_back(num[i]);
+	}
+	
+	cout << "初始化 L1 =" << endl;
+	printList(L1);
+
+	//初始化L2
+	for (int i = 0; i < 5; i++)
+	{
+		L2.push_front(num[i]);
+	}
+
+	cout << "初始化 L2 =" << endl;
+	printList(L2);
+
+	cout << "尾删 L1" << endl;
+	L1.pop_back();
+	printList(L1);
+	cout << "头删" << endl;
+	L2.pop_front();
+	printList(L2);
+
+	list<int>::iterator it = L1.begin();
+	//注意 list 遍历访问不能跳 例如 it + 10 是违法的
+	// list 是双向循环链表 记住！！！
+	L1.insert(++it, 2);
+	cout << "begin+1 位置插入2" << endl;
+	printList(L1);
+	//实际上it已经访问到begin + 2 的位置了
+	L1.erase(it);
+	cout << "擦除begin + 2位置" << endl;
+	printList(L1);
+	//移除元素为  1  的值
+	L1.remove(2);
+	cout << "移除元素为2的值:" << endl;
+	printList(L1);
+	L1.clear();
+	printList(L1);
+
+	cout << "L2的头元素 " << L2.front() << endl;
+	cout << "L2的尾元素 " << L2.back()	<< endl;
+	
+	L2.reverse();
+	cout << "L2 反转链表" << endl;
+	printList(L2);
+
+	L2.sort();
+	cout << "L2 升序" << endl;
+	printList(L2);
+	L2.sort(cmp);
+	cout << "L2 降序" << endl;
+	printList(L2);
+}
+
+bool cmp(int a, int b)
+{
+	return a > b;
 }
 
 bool ComparePerson(Person& p1, Person& p2)
