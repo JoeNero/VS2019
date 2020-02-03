f6aa351b4d7be9e5a54ea0a2fcbc25bc4aed1e65
526988861@qq.com <526988861@qq.com>
2020-02-03 20:25:13 +0800

    Signed-off-by: 526988861@qq.com <526988861@qq.com>

---
+++ 容器demo/mPair.cpp
@@ -0,0 +1,25 @@
+#include <string>
+//#include <utility>
+#include <iostream>
+
+using namespace std;
+
+void test01()
+{
+	pair<string, int> p(string("Tom"), 20);
+
+	cout << "姓名:" << p.first << "年龄:" << p.second << endl;
+
+	pair<string, int> p2 = make_pair("jerry", 20);
+
+	cout << "姓名:" << p2.first << "年龄:" << p2.second << endl;
+}
+
+int main()
+{
+	test01();
+	system("pause");
+
+	return 0;
+
+}
