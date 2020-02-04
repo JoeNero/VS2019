6af4c79444c5e2506a5cdbf7a81b935ac454e39f
526988861@qq.com <526988861@qq.com>
2020-02-04 15:48:14 +0800

    Signed-off-by: 526988861@qq.com <526988861@qq.com>

--- 算法/mAlgorithm4.cpp
+++ 算法/mAlgorithm4.cpp
@@ -25,6 +25,15 @@
 			end2 容器2结束迭代器 
 			dest 目标容器开始迭代器
 			-------------------------------------------------------------
+			set_difference(iterator beg1, iterator end1, iterator beg2, iterator end2, iterator dest);
+			求两个集合的差集(前 - 后)
+			注意:两个集合必须是有序序列
+			beg1 容器1开始迭代器 
+			end1 容器1结束迭代器 
+			beg2 容器2开始迭代器 
+			end2 容器2结束迭代器 
+			dest 目标容器开始迭代器
+			-------------------------------------------------------------
 时间	:	2020/01/08
 */
 
@@ -95,5 +104,17 @@ int main()
 	vector<int>::iterator itEnd = 
 	set_union(v1.begin(), v1.end(), v2.begin(), v2.end(), vT.begin());
 	for_each(vT.begin(),itEnd,MyPrint());
+	cout << endl;
+
+	//求差集
+	vT.resize(max(v1.size() , v2.size()));
+	cout << "v1与v2差集" << endl;
+	itEnd =set_difference(v1.begin(), v1.end(), v2.begin(), v2.end(), vT.begin());
+	for_each(vT.begin(), itEnd, MyPrint());
+	cout << endl;
+
+	cout << "v2与v1差集" << endl;
+	itEnd = set_difference(v2.begin(), v2.end(), v1.begin(), v1.end(), vT.begin());
+	for_each(vT.begin(), itEnd, MyPrint());
 	cout << endl;
 }
